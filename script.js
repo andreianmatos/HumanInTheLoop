086ce291c81040cd70657de88ac00eefae3b4051
andreianmatos <58214313+andreianmatos@users.noreply.github.com>
2021-11-23 19:56:41 +0100

    alternative

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // before 
 
 function start() {
-
 }   
 
 // other functions 
@@ -15,3 +14,17 @@ function show(item){
     document.getElementById(item).style.display="none";
   }
 }
+
+function notification(){
+  var x = document.getElementById("txt");
+  setTimeout(() => {  x.style.display = "block"; x.innerHTML = "NOTIFICATION ONE";}, 4000);
+  setTimeout(() => {  x.style.display = "none";}, 6000);
+  setTimeout(() => { x.style.display = "block"; x.innerHTML = "NOTIFICATION TWOO" }, 10000);
+  setTimeout(() => {  x.style.display = "none";}, 12000);
+  setTimeout(() => { x.style.display = "block"; x.innerHTML = "NOTIFICATION FINAL" }, 20000);
+  setTimeout(() => {  x.style.display = "none";}, 22000);
+}
+
+
+
+
