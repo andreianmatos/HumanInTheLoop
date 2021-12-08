4a7c037e4b9e4ca4c322f02d8c79ba8c8456e398
andreianmatos <58214313+andreianmatos@users.noreply.github.com>
2021-12-08 17:51:21 +0100

    changes

--- script.js
+++ script.js
@@ -15,20 +15,24 @@ function show(item){
   }
 }
 
+
+
 function notification(){
   var x = document.getElementById("txt");
   var s;
   var popup = document.getElementById("donePopup");
 
-  setTimeout(() => {  s = document.getElementById("sound1"); s.play(); x.style.display = "block"; x.innerHTML = "NOTIFICATION ONE";}, 4000);
-  setTimeout(() => {  x.style.display = "none";}, 6000);
-  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "NOTIFICATION TWOO" }, 10000);
-  setTimeout(() => {  x.style.display = "none";}, 12000);
-  setTimeout(() => {  s = document.getElementById("sound3"); s.play(); x.style.display = "block"; x.innerHTML = "NOTIFICATION FINAL" }, 20000);
-  setTimeout(() => {  x.style.display = "none";}, 22000);
+  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Alice: Hello There …";}, 120000);
+  setTimeout(() => {  x.style.display = "none";}, 125000);
+  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Bob: It’s 18h56…" }, 480000);
+  setTimeout(() => {  x.style.display = "none";}, 485000);
+  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Charlie: Orange is great …"},  660000);
+  setTimeout(() => {  x.style.display = "none";},  665000);
+  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Danny : Meet Wednesday ?"}, 780000);
+  setTimeout(() => {  x.style.display = "none";}, 785000);
 
   // pop up after video is over, put size of video in miliseconds
-  setTimeout(() => { popup.style.display = "block"; popup.innerHTML = '<p>The video is over!</p><a type="button" class="custombutton" href="endPage.html">NEXT</a>';}, 25000);
+  setTimeout(() => { popup.style.display = "block"; popup.innerHTML = '<p>The video is over!</p><a type="button" class="custombutton" href="endPage.html">NEXT</a>';}, 933600 );
   
 }
 
