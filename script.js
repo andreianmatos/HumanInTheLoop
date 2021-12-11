b3a113f9804d553a297bdb9c7997b8f9de4d7d06
andreianmatos <58214313+andreianmatos@users.noreply.github.com>
2021-12-11 11:19:54 +0100

    s

--- script.js
+++ script.js
@@ -21,21 +21,42 @@ function notification(){
   var x = document.getElementById("txt");
   var s;
   var popup = document.getElementById("donePopup");
+  var soundType;
 
-  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Alice: Hello There …";}, 120000);
-  setTimeout(() => {  x.style.display = "none";}, 125000);
-  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Bob: It’s 18h56…" }, 480000);
-  setTimeout(() => {  x.style.display = "none";}, 485000);
-  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Charlie: Orange is great …"},  660000);
-  setTimeout(() => {  x.style.display = "none";},  665000);
-  setTimeout(() => {  s = document.getElementById("sound2"); s.play(); x.style.display = "block"; x.innerHTML = "Danny : Meet Wednesday ?"}, 780000);
-  setTimeout(() => {  x.style.display = "none";}, 785000);
+  if(localStorage.getItem("groupType") == 'A'){
+    setTimeout(() => {  x.style.display = "block"; x.innerHTML = "<b>Alice</b>: Hello There …";}, 120000);
+    setTimeout(() => {  x.style.display = "none";}, 125000);
+    setTimeout(() => {  x.style.display = "block"; x.innerHTML = "<b>Bob</b>: It’s 18h56…" }, 480000);
+    setTimeout(() => {  x.style.display = "none";}, 485000);
+    setTimeout(() => {  x.style.display = "block"; x.innerHTML = "<b>Charlie</b>: Orange is great …"},  660000);
+    setTimeout(() => {  x.style.display = "none";},  665000);
+    setTimeout(() => {  x.style.display = "block"; x.innerHTML = "<b>Danny</b>: Meet Wednesday ?"}, 780000);
+    setTimeout(() => {  x.style.display = "none";}, 785000);  
+  }
+
+  if(localStorage.getItem("groupType") != 'A'){
+
+    if(localStorage.getItem("groupType") == 'B'){
+    soundType = "sound1";
+    }
+    if(localStorage.getItem("groupType") == 'C'){
+    soundType = "sound2";
+    }
+
+    setTimeout(() => {  s = document.getElementById(soundType); s.play(); x.style.display = "block"; x.innerHTML = "<b>Alice</b>: Hello There …";}, 120000);
+    setTimeout(() => {  x.style.display = "none";}, 125000); 
+    setTimeout(() => {  s = document.getElementById(soundType); s.play(); x.style.display = "block"; x.innerHTML = "<b>Bob</b>: It’s 18h56…" }, 480000);
+    setTimeout(() => {  x.style.display = "none";}, 485000);
+    setTimeout(() => {  s = document.getElementById(soundType); s.play(); x.style.display = "block"; x.innerHTML = "<b>Charlie</b>: Orange is great …"},  660000);
+    setTimeout(() => {  x.style.display = "none";},  665000);
+    setTimeout(() => {  s = document.getElementById(soundType); s.play(); x.style.display = "block"; x.innerHTML = "<b>Danny</b>: Meet Wednesday ?"}, 780000);
+    setTimeout(() => {  x.style.display = "none";}, 785000);  
+  }
 
   // pop up after video is over, put size of video in miliseconds
   setTimeout(() => { popup.style.display = "block"; popup.innerHTML = '<p>The video is over!</p><a type="button" class="custombutton" href="endPage.html">NEXT</a>';}, 939600 );
-  
 }
 
-
-
-
+function groupSetting(group) {
+  localStorage.setItem("groupType", group);
+}
